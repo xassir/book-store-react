558d8e244afda20a2b55e01503f632a8dd55913f
yasir.raja <yasir.asghar@zameen.com>
2018-01-02 18:01:32 +0500

    2nd commit

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginPage = () => (
+    <div>
+    <h1> Login Page </h1>
+    <p>sdhygfusd gfusdgfyu du</p>
+    <Link to="/" >Home</Link>
+    <Test />
+    </div>
+);
+
+
+const Test = () => (
+    <div>
+    <p>Yasir</p>
+    </div>
+);
+
+export default LoginPage; 
